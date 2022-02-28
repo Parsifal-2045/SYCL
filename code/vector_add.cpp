0b1c99dc4efe34f41a8eecd6bd13c1330d9716ec
Luca <ferragina.luca@gmail.com>
2022-02-28 19:09:39 +0100

    Fixed spacing

--- code/vector_add.cpp
+++ code/vector_add.cpp
@@ -1,23 +1,25 @@
 #include <CL/sycl.hpp>
 #include <cassert>
 
-
 int main()
 {
     constexpr size_t N = 1024;
     std::vector<float> a(N);
     std::vector<float> b(N);
     std::vector<float> c(N);
+    
     for (int i = 0; i != N; i++)
     {
         a[i] = static_cast<float>(i);
         b[i] = static_cast<float>(i);
         c[i] = 0.0f;
     }
+
     auto buf_a = sycl::buffer<float, 1>(a.data(), sycl::range<1>(N * sizeof(float)));
     auto buf_b = sycl::buffer<float, 1>(b.data(), sycl::range<1>(N * sizeof(float)));
     auto buf_c = sycl::buffer<float, 1>(c.data(), sycl::range<1>(N * sizeof(float)));
     auto queue = sycl::queue{sycl::default_selector{}};
+
     queue.submit([&](sycl::handler &cgh)
     {
         auto in_a = buf_a.get_access<sycl::access::mode::read>(cgh);
