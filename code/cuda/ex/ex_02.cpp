1f20979fbb6175a3c0989e02a325010f71e04736
Luca <ferragina.luca@gmail.com>
2022-03-16 19:02:29 +0100

    Modified solution to use bidimensional range

--- code/cuda/ex/ex_02.cpp
+++ code/cuda/ex/ex_02.cpp
@@ -4,27 +4,33 @@
 
 int main()
 {
-    int N = 1024;
+    int numBlocks = 8;
+    int numThreadsPerBlock = 8;
+    int N = numBlocks * numThreadsPerBlock;
     std::vector<int> h_a(N); 
     size_t memSize = N * sizeof(int);
     auto queue = sycl::queue{sycl::default_selector{}};
     auto d_a = sycl::malloc_device<int>(N, queue);
+   
     queue.memcpy(d_a, h_a.data(), memSize);
     queue.submit([&](sycl::handler &cgh)
     {
-        cgh.parallel_for(sycl::range<1>(N), [=](sycl::id<1> idx) 
+        cgh.parallel_for(sycl::range<2>(numBlocks, numThreadsPerBlock), [=](sycl::id<2> id) 
         { 
-            auto i = idx[0];
-            d_a[i] = i + 42;
+            auto index = id[0] * numThreadsPerBlock + id[1];
+            d_a[index] = id[0] + id[1] + 42;
         });
     }).wait();
 
     queue.memcpy(h_a.data(), d_a, memSize).wait();
     sycl::free(d_a, queue);
 
-    for(int i = 0; i != N; i++)
+    for(int i = 0; i != numBlocks; i++)
     {
-        assert(h_a[i] == i + 42);
+        for(int j = 0; j != numThreadsPerBlock; j++)
+        {
+            assert(h_a[i * numThreadsPerBlock + j] == i + j + 42);
+        }
     }
 
     std::cout << "Correct!" << '\n';
