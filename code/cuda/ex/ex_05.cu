06e95b18fa556a1bcd8b90f02ffc9b85825451ec
Parsifal-2045 <ferragina.luca@gmail.com>
2022-03-26 13:04:45 +0100

    Updated (and simplified?) version of parallel reduction (not tested)

--- code/cuda/ex/ex_05.cu
+++ code/cuda/ex/ex_05.cu
@@ -6,35 +6,31 @@
 // Here you can set the device ID that was assigned to you
 #define MYDEVICE 0
 
-static const int num_elements = 1 << 18;
-static const int block_size = 1024;
-static const int grid_size = 32;
+static const int num_elements = 2 << 20;
+static const int threads = 1024;
+static const int blocks = num_elements / threads;
 
 // Part 1 of 6: implement the kernel
-__global__ void block_sum(const int *input, int *per_block_results, const size_t n)
+__global__ void block_sum(const int *input, int *result)
 {
-    int idx = threadIdx.x;
-    int gidx = idx + blockIdx.x * block_size;
-    const int gridSize = block_size * gridDim.x;
-    int sum = 0;
-    for (int i = gidx; i < num_elements; i += gridSize)
-    {
-        sum += input[i];
-    }
-    __shared__ int sdata[block_size];
-    sdata[idx] = sum;
+    __shared__ int sdata[threads];
+    int id = threadIdx.x + blockDim.x * blockIdx.x;
+    int tid = threadIdx.x;
+
+    sdata[tid] = input[id];
     __syncthreads();
-    for (int size = grid_size / 2; size > 0; size /= 2)
+
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>=1)
     {
-        if (idx < size)
+        if (tid < s)
         {
-            sdata[idx] += sdata[idx + size];
-            __syncthreads();
+            sdata[tid] += sdata[tid + s];
         }
+        __syncthreads();
     }
-    if (idx == 0)
+    if (tid == 0)
     {
-        per_block_results[blockIdx.x] = sdata[0];
+        result[blockIdx.x] = sdata[0];
     }
 }
 
@@ -60,31 +56,21 @@ int main(void)
     const int host_result = std::accumulate(h_input.begin(), h_input.end(), 0);
     std::cerr << "Host sum: " << host_result << std::endl;
 
-    // //Part 1 of 6: move input to device memory
     int *d_input;
     size_t memSize = num_elements * sizeof(int);
     cudaMalloc(&d_input, memSize);
     cudaMemcpy(d_input, h_input.data(), memSize, cudaMemcpyHostToDevice);
 
-    // // Part 1 of 6: allocate the partial sums: How much space does it need?
-    int *d_partial_sums_and_total;
-    size_t partial_mem = grid_size * sizeof(int) ;
-    cudaMalloc(&d_partial_sums_and_total, partial_mem);
-
-    // // Part 1 of 6: launch one kernel to compute, per-block, a partial sum. How
-    // much shared memory does it need?
-    block_sum<<<grid_size, block_size>>>(d_input, d_partial_sums_and_total, num_elements);
-    // d_partial_sums_and_total holds the partial result
-    block_sum<<<1, block_size>>>(d_partial_sums_and_total, d_partial_sums_and_total, num_elements);
-    // d_partial_sums_and_total[0] holds the final result
-    cudaDeviceSynchronize();
+    int *d_result;
+    cudaMalloc(&d_result, sizeof(int));
+    block_sum<<<blocks, threads>>>(d_input, d_result);
     int device_result;
-    cudaMemcpy(&device_result, d_partial_sums_and_total, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&device_result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
 
     std::cout << "Device sum: " << device_result << std::endl;
 
     // // Part 1 of 6: deallocate device memory
     cudaFree(d_input);
-    cudaFree(d_partial_sums_and_total);
+    cudaFree(d_result);
     return 0;
 }
