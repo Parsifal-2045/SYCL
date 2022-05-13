16148401033b83e4423e331d834146256c4a2102
Parsifal-2045 <ferragina.luca@gmail.com>
2022-05-13 19:28:15 +0200

    Implemented calculate_distanceToHigher

--- code/clue_OneAPI/src/CLUEAlgoOneAPI.cpp
+++ code/clue_OneAPI/src/CLUEAlgoOneAPI.cpp
@@ -74,67 +74,68 @@ void kernel_calculate_density(LayerTilesGPU *d_hist, PointsPtr d_points, float d
   }).wait();
 }
 
-__global__ void kernel_calculate_distanceToHigher(LayerTilesGPU *d_hist,
-                                                  PointsPtr d_points,
-                                                  float outlierDeltaFactor,
-                                                  float dc,
-                                                  int numberOfPoints)
+void kernel_calculate_distanceToHigher(LayerTilesGPU *d_hist, PointsPtr d_points, float outlierDeltaFactor, float dc, int numberOfPoints)
 {
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-
-  float dm = outlierDeltaFactor * dc;
-
-  if (i < numberOfPoints)
+  auto queue = sycl::queue{Intel_gpu_selector{}};
+  queue.submit([&](sycl::handler &cgh)
   {
-    int layeri = d_points.layer[i];
-
-    float deltai = std::numeric_limits<float>::max();
-    int nearestHigheri = -1;
-    float xi = d_points.x[i];
-    float yi = d_points.y[i];
-    float rhoi = d_points.rho[i];
-
-    // get search box
-    int4 search_box = d_hist[layeri].searchBox(xi - dm, xi + dm, yi - dm, yi + dm);
-
-    // loop over all bins in the search box
-    for (int xBin = search_box.x; xBin < search_box.y + 1; ++xBin)
+    cgh.parallel_for(sycl::range<1>(numberOfPoints), [=](sycl::id<1> idx)
     {
-      for (int yBin = search_box.z; yBin < search_box.w + 1; ++yBin)
+      int i = idx[0];
+      float dm = outlierDeltaFactor * dc;
+      if (i < numberOfPoints)
       {
-        // get the id of this bin
-        int binId = d_hist[layeri].getGlobalBinByBin(xBin, yBin);
-        // get the size of this bin
-        int binSize = d_hist[layeri][binId].size();
+        int layeri = d_points.layer[i];
+        float deltai = std::numeric_limits<float>::max();
+        int nearestHigheri = -1;
+        float xi = d_points.x[i];
+        float yi = d_points.y[i];
+        float rhoi = d_points.rho[i];
 
-        // interate inside this bin
-        for (int binIter = 0; binIter < binSize; binIter++)
+        // get search box
+        int4 search_box = d_hist[layeri].searchBox(xi - dm, xi + dm, yi - dm, yi + dm);
+
+        // loop over all bins in the search box
+        for (int xBin = search_box.x; xBin < search_box.y + 1; xBin++)
         {
-          int j = d_hist[layeri][binId][binIter];
-          // query N'_{dm}(i)
-          float xj = d_points.x[j];
-          float yj = d_points.y[j];
-          float dist_ij = std::sqrt((xi - xj) * (xi - xj) + (yi - yj) * (yi - yj));
-          bool foundHigher = (d_points.rho[j] > rhoi);
-          // in the rare case where rho is the same, use detid
-          foundHigher = foundHigher || ((d_points.rho[j] == rhoi) && (j > i));
-          if (foundHigher && dist_ij <= dm)
-          { // definition of N'_{dm}(i)
-            // find the nearest point within N'_{dm}(i)
-            if (dist_ij < deltai)
+          for (int yBin = search_box.z; yBin < search_box.y + 1; yBin++)
+          {
+            // get the id of this bin
+            int binId = d_hist[layeri].getGlobalBinByBin(xBin, yBin);
+            // get the size of this bin
+            int binSize = d_hist[layeri][binId].size();
+
+            //iterate inside this bin
+            for (int binIter = 0; binIter < binSize; binIter++)
             {
-              // update deltai and nearestHigheri
-              deltai = dist_ij;
-              nearestHigheri = j;
-            }
+              int j = d_hist[layeri][binId][binIter];
+              // query N'_{dm}(i)
+              float xj = d_points.x[j];
+              float yj = d_points.y[j];
+              float dist_ij = std::sqrt((xi - xj) * (xi - xj) + (yi - yj) * (yi - yj));
+              bool foundHigher = (d_points.rho[j] > rhoi);
+              // in the rare case where rho is the same, use detid
+              foundHigher = foundHigher || ((d_points.rho[j] == rhoi) && (j > i));
+              if (foundHigher && dist_ij <= dm)
+              {
+                // definitio of N'_{dm}(i)
+                // find the nearest point within N'_{dm}(i)
+                if (dist_ij < deltai)
+                {
+                  // update deltai and nearestHigheri
+                  deltai = dist_ij;
+                  nearestHigheri = j;
+                }
+              }
+            } // end of iterate inside this bin
           }
-        } // end of interate inside this bin
+        } // end of loop over bins in search box
+        d_points.delta[i] = deltai;
+        d_points.nearestHigher[i] = nearestHigheri;
       }
-    } // end of loop over bins in search box
-    d_points.delta[i] = deltai;
-    d_points.nearestHigher[i] = nearestHigheri;
-  }
-} // kernel
+    });
+  }).wait(); 
+}
 
 __global__ void kernel_find_clusters(GPU::VecArray<int, maxNSeeds> *d_seeds,
                                      GPU::VecArray<int, maxNFollowers> *d_followers,
