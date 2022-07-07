225dc98eae73774ee70f0e680a000e7860aa819f
Parsifal-2045 <ferragina.luca@gmail.com>
2022-07-07 11:12:38 +0200

    Implemented better solution

--- code/cuda/ex/ex_05.cpp
+++ code/cuda/ex/ex_05.cpp
@@ -30,23 +30,26 @@ void block_sum(const int* input, int* output, sycl::nd_item<3> item, int* sdata)
         output[item.get_group(0)] = sdata[0];
     }
 
-    item.barrier();
 }
 
 int main()
 {
     auto queue = sycl::queue(sycl::gpu_selector{});
-    int numInputElements = 2135;
+    int numInputElements = 2048;
     int numOutputElements;
     numOutputElements = numInputElements / (numThreadsPerBlock / 2);
     if (numInputElements % (numThreadsPerBlock / 2))
     {
         numOutputElements++;
     }
-    std::vector<int> h_input(numInputElements);    
+    std::vector<int> h_input;
+
     for (int i = 0; i != numInputElements; i++)
     {
-        h_input[i] = 1;        
+        std::mt19937 gen(std::random_device{}());
+        std::uniform_real_distribution<> dis(0, 100);
+        auto value = dis(gen);
+        h_input.push_back(value);
     }
 
     const int host_result = std::accumulate(h_input.begin(), h_input.end(), 0);
@@ -54,20 +57,15 @@ int main()
     const sycl::range<3> blockSize(numThreadsPerBlock, 1, 1);
     const sycl::range<3> gridSize(numOutputElements, 1, 1);
 
-    int* d_input;
-    d_input = sycl::malloc_device<int>(numInputElements, queue);
+    int* d_input = sycl::malloc_device<int>(numInputElements, queue);
     queue.memcpy(d_input, h_input.data(), numInputElements * sizeof(int)).wait();
 
-    std::vector<int> zero(numOutputElements);
-
-    int* d_output;
-    d_output = sycl::malloc_device<int>(numOutputElements, queue);   
-    queue.memcpy(d_output, zero.data(), numOutputElements * sizeof(int)).wait();
-
+    int* d_output = sycl::malloc_device<int>(numOutputElements, queue);   
+    
     std::cout << blockSize.get(0) << '\n';
     std::cout << gridSize.get(0) << '\n';
 
-    queue.submit([&](sycl::handler &cgh) 
+    queue.submit([&](sycl::handler& cgh) 
     {
         sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::local> sdata(sycl::range<1>(numThreadsPerBlock), cgh);
         cgh.parallel_for(sycl::nd_range<3>(gridSize * blockSize, blockSize), [=](sycl::nd_item<3> item)
@@ -76,17 +74,13 @@ int main()
         });
     }).wait();
 
-    int device_result[numOutputElements];
-    queue.memcpy(&device_result, d_output, numOutputElements * sizeof(int)).wait();
-    for (int i = 1; i != numOutputElements; i++)
-    {
-        device_result[0] += device_result[i];
-    }
+    std::vector<int> device_output(numOutputElements);
+    queue.memcpy(device_output.data(), d_output, numOutputElements * sizeof(int)).wait();
 
+    int device_result = std::accumulate(device_output.begin(), device_output.end(), 0);
     std::cout << "Host sum: " << host_result << std::endl;
-    std::cout << "Device sum: " << device_result[0] << std::endl;
-
+    std::cout << "Device sum: " << device_result << std::endl;
+    
     sycl::free(d_input, queue);
     sycl::free(d_output, queue);
-    return 0;
 }
